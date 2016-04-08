27fce0ce22709691ff8b708d30747c801bacde60
will <nicewoog@naver.com>
2016-04-09 00:12:30 +0900

    두번째 커밋
    페이지의 개략적인 틀을 잡고 CSS를 적용시켰고, CSS 적용방법 및 네비게이션 바 만들기,  Div태그를 테이블로 만드는 방법을
    추가

--- app.js
+++ app.js
@@ -3,12 +3,23 @@ var path = require('path');
 var fs = require('fs');
 
 var app = express();
+app.use(express.static(path.join(__dirname, 'assets/css')));
 
-app.get('/', function(req, res) {
+app.get('/index', function(req, res) {
   res.set('content-type','text/html');
   res.send(fs.readFileSync(__dirname+'/index.html','utf8'));
   res.end();
   console.log('execute');
 });
+
+app.get('/node', function(req, res){
+  res.set('content-type','text/html');
+  res.send(fs.readFileSync(__dirname+'/node.html','utf8'));
+  res.end();
+})
+
+app.get('*', function(req, res){
+  res.send('잘못된 접근 혹은 정확하지 않은 url입니다.<br> /index로 접속해주세요.');
+})
 
 app.listen(3000);
