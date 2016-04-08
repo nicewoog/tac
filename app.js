4f076461d2824de2dd2c9c8f595056b384a3ea1a
will <nicewoog@naver.com>
2016-04-09 01:46:02 +0900

    두번째 커밋(조금 추가)
    오늘의 마무리를 하며... node에 대한 내용 추가

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ var path = require('path');
 var fs = require('fs');
 
 var app = express();
+
 app.use(express.static(path.join(__dirname, 'assets/css')));
+app.use(express.static(path.join(__dirname, 'assets/js')));
+app.use(express.static(path.join(__dirname, 'assets/img')));
 
 app.get('/index', function(req, res) {
   res.set('content-type','text/html');
@@ -12,10 +15,14 @@ app.get('/index', function(req, res) {
   console.log('execute');
 });
 
-app.get('/node', function(req, res){
+app.get('/getContentAjax', function(req, res){
+  var content = fs.readFileSync(__dirname+'/node.html','utf8')
+  var sendData = {'nodeHtml':content}
+
   res.set('content-type','text/html');
-  res.send(fs.readFileSync(__dirname+'/node.html','utf8'));
+  res.send(sendData);
   res.end();
+  console.log('send some data for client request : ' + sendData);
 })
 
 app.get('*', function(req, res){
